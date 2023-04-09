ee87b08dd1af71ec62e1643289c418e4d5f53f97
hitcomvn <130174322+hitcomvn@users.noreply.github.com>
2023-04-09 15:20:11 +0700

    Update my-localtion.php

--- my-localtion.php
+++ my-localtion.php
@@ -127,6 +127,51 @@ function my_location_display_user_location( $use_geo_html_5 = true ) {
         echo '<div id="my-location-source">' . $source . '</div>';
     }
 }
+// Register plugin configuration menu
+function my_location_register_settings_page() {
+    add_options_page('My Location Settings', 'My Location', 'manage_options', 'my-location', 'my_location_settings_page');
+}
+add_action('admin_menu', 'my_location_register_settings_page');
+// Save user location information to log file
+function my_location_save_to_log() {
+    $ip_address = $_SERVER['REMOTE_ADDR'];
+    $latitude = '';
+    $longitude = '';
+    $source = 'HTML5 Geolocation';
+
+    // Try to get location using HTML5 Geolocation
+    if (isset($_COOKIE['my_location_latitude']) && isset($_COOKIE['my_location_longitude'])) {
+        $latitude = $_COOKIE['my_location_latitude'];
+        $longitude = $_COOKIE['my_location_longitude'];
+    } else {
+        // If HTML5 Geolocation is not available, get location using Google Maps API
+        $location = my_location_get_location_by_ip($ip_address);
+        $latitude = $location['latitude'];
+        $longitude = $location['longitude'];
+        $source = 'Google Maps API';
+    }
+
+    $time = current_time('mysql');
+    $data = "{$time}\t{$ip_address}\t{$latitude}\t{$longitude}\t{$source}\n";
+    $log_file = plugin_dir_path(__FILE__) . 'log.txt';
+    file_put_contents($log_file, $data, FILE_APPEND);
+}
+add_action('init', 'my_location_save_to_log');
+// Display plugin configuration page
+function my_location_settings_page() {
+    ?>
+    <div class="wrap">
+        <h1>My Location Settings</h1>
+        <form method="post" action="options.php">
+            <?php
+            settings_fields('my_location_settings');
+            do_settings_sections('my_location_settings');
+            submit_button();
+            ?>
+        </form>
+    </div>
+    <?php
+}
 
 /**
  * Update the user's location via AJAX
@@ -136,8 +181,44 @@ function my_location_update_user_location() {
     $latitude = isset( $_POST['latitude'] ) ? sanitize_text_field( $_POST['latitude'] ) : '';
     $longitude = isset( $_POST['longitude'] ) ? sanitize_text_field( $_POST['longitude'] ) : '';
 
-    // Update the user's latitude and longitude in the database.
-    $user_id = get_current_user_id();
-    update_user_meta( $user_id, 'my_location_latitude', $latitude
+    // Try to get the user's location using Google Maps API if HTML5 Geolocation is not available
+    if ( empty( $latitude ) || empty( $longitude ) ) {
+        $geolocation = my_location_get_user_location_by_google_maps_api();
 
-function my_location_init
+        if ( ! is_wp_error( $geolocation ) ) {
+            $latitude = $geolocation['latitude'];
+            $longitude = $geolocation['longitude'];
+        }
+    }
+
+    // Update the user's latitude and longitude in the database.
+    if ( ! empty( $latitude ) && ! empty( $longitude ) ) {
+        $user_id = get_current_user_id();
+        update_user_meta( $user_id, 'my_location_latitude', $latitude );
+        update_user_meta( $user_id, 'my_location_longitude', $longitude );
+    }
+
+    // Return a success response.
+    wp_send_json_success();
+}
+
+function my_location_init() {
+    add_action( 'wp_enqueue_scripts', 'my_location_enqueue_scripts' );
+
+    add_shortcode( 'my_location', 'my_location_display_user_location_shortcode' );
+
+    add_action( 'admin_menu', 'my_location_add_admin_menu' );
+    add_action( 'admin_init', 'my_location_settings_init' );
+
+    // Try to get the user's location from Google Maps API if the API key is set.
+    $api_key = get_option( 'my_location_api_key' );
+    if ( ! empty( $api_key ) ) {
+        add_action( 'wp_footer', 'my_location_get_user_location_google_maps', 20 );
+    } else {
+        add_action( 'wp_footer', 'my_location_get_user_location_html5', 20 );
+    }
+
+    // Log user location data.
+    add_action( 'wp_footer', 'my_location_log_user_location', 30 );
+}
+
