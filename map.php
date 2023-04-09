cf607e9bac7894911ecfe86073bf6092591ba07e
hitcomvn <130174322+hitcomvn@users.noreply.github.com>
2023-04-09 23:16:11 +0700

    Update map.php

--- map.php
+++ map.php
@@ -1,98 +1,74 @@
 <?php
 /*
-Plugin Name: Map Location
-Description: Get user location using HTML5 Geolocation and Google Maps API
+Plugin Name: My Location Plugin
+Plugin URI: https://example.com
+Description: A plugin to show user location and save it to log file.
 Version: 1.0
+Author: Your Name
+Author URI: https://example.com
 */
 
-// Require user to share location to access website
+// Register plugin activation hook
+register_activation_hook(__FILE__, 'my_location_plugin_activate');
 
-function require_location() {
-  if(!isset($_COOKIE['user_location'])) { // Check if user location cookie exists
-    setcookie('user_location', 'allowed', time() + (86400 * 30), '/'); // Set user location cookie for 30 days
-    echo '<script>
+// Function to activate plugin
+function my_location_plugin_activate() {
+    // Create log file
+    $file = fopen(plugin_dir_path(__FILE__) . 'log.txt', 'w');
+    fclose($file);
+}
+
+// Register shortcode to display location button
+add_shortcode('location_button', 'my_location_button');
+
+// Function to display location button
+function my_location_button() {
+    // Check if user location cookie exists
+    if (!isset($_COOKIE['user_location'])) {
+        // Display location button
+        return '<button onclick="getLocation()">Đồng ý chia sẻ vị trí</button>';
+    }
+}
+
+// Add JavaScript to get user location and save to log file
+add_action('wp_footer', 'my_location_script');
+
+// Function to add JavaScript
+function my_location_script() {
+    // Check if user location cookie exists
+    if (!isset($_COOKIE['user_location'])) {
+        // Add JavaScript
+        echo '
+        <script>
+        function getLocation() {
             if (navigator.geolocation) {
-              navigator.geolocation.getCurrentPosition(function(position) {
-                console.log("User location (GEO): ", position.coords.latitude, position.coords.longitude);
-                save_location({
-                  latitude: position.coords.latitude,
-                  longitude: position.coords.longitude
-                }, "gps");
-              });
+                navigator.geolocation.getCurrentPosition(showPosition);
+            } else {
+                alert("Geolocation is not supported by this browser.");
             }
-            else {
-              alert("Trình duyệt của bạn không hỗ trợ chia sẻ vị trí.");
-            }
-          </script>';
-  }
-}
-add_action('wp_head', 'require_location');
-// Update user location cookie to "allowed"
-
-// Get user location using HTML5 Geolocation
-function get_html5_location() {
-  if(isset($_COOKIE['user_location']) && $_COOKIE['user_location'] == 'allowed') { // Check if user allowed to share location
-    echo '<script>
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-          var location = {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-          };
-          console.log("User location (HTML5): ", location);
-          save_location(location, "gps");
-        });
-      }
-    </script>';
-  }
-}
-
-// Get user location using Google Maps API
-function get_apikey_location() {
-  if(isset($_COOKIE['user_location']) && $_COOKIE['user_location'] == 'allowed') { // Check if user allowed to share location
-    $api_key = 'AIzaSyBs5CTk8t1VvTKyTYZ7dIwyd4WetqW7jLc'; // Replace with your Google Maps API key
-    echo '<script src="https://maps.googleapis.com/maps/api/js?key=' . $api_key . '&callback=initMap" async defer></script>
-    <script>
-      function initMap() {
-        var geocoder = new google.maps.Geocoder();
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var location = {
-              latitude: position.coords.latitude,
-              longitude: position.coords.longitude
-            };
-            geocoder.geocode({"location": location}, function(results, status) {
-              if (status === "OK") {
-                console.log("User location (API): ", results[0].geometry.location);
-                save_location(results[0].geometry.location, "apikey");
-              } else {
-                console.log("Geocode failed: " + status);
-              }
-            });
-          });
         }
-      }
-    </script>';
-  }
+
+        function showPosition(position) {
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
+            var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&key=AIzaSyBs5CTk8t1VvTKyTYZ7dIwyd4WetqW7jLc";
+
+            // Send request to Google Maps API
+            fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                // Save user location to log file
+                var address = data.results[0].formatted_address;
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", "' . plugin_dir_url(__FILE__) . 'save_location.php", true);
+                xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                xhr.send("location=" + address);
+            })
+            .catch(error => {
+                alert("Error: " + error);
+            });
+        }
+        </script>
+        ';
+    }
 }
-
-
-
-
-function save_location($location, $type) {
-  $log_file = 'location_log.txt'; // Set log file name
-  $file = fopen($log_file, 'a'); // Open log file in append mode
-  $time = date('Y-m-d H:i:s'); // Get current time
-  $data = $time . ',' . $type . ',' . $location['latitude'] . ',' . $location['longitude'] . "\n"; // Format data to be logged
-  fwrite($file, $data); // Write data to log file
-  fclose($file); // Close log file
-}
-
-if(isset($_COOKIE['user_location']) && $_COOKIE['user_location'] == 'allowed') { // Check if user allowed to share location
-  if(isset($_GET['apikey'])) { // Check if API key location is set
-    get_apikey_location(); // Get user location using API key
-  } else {
-    get_geolocation(); // Get user location using HTML5 geolocation
-  }
-}
-
