bc9a75c72ddccdee2230d06298532daee047061c
hitcomvn <130174322+hitcomvn@users.noreply.github.com>
2023-04-09 21:14:27 +0700

    Update map.php

--- map.php
+++ map.php
@@ -11,6 +11,33 @@ License: GPL2
 
 // Add action to run the function when WordPress initializes
 
+function require_location() {
+    if (isset($_COOKIE['user_location'])) {
+        return;
+    }
+
+    // Check if user has shared their location
+    if (isset($_POST['latitude']) && isset($_POST['longitude'])) {
+        $lat = $_POST['latitude'];
+        $lng = $_POST['longitude'];
+        setcookie('user_location', $lat . ',' . $lng, time() + 3600);
+        return;
+    }
+
+    // Check if user's browser supports geolocation
+    if (function_exists('geoip_detect2_get_info_from_current_ip')) {
+        $user_location = geoip_detect2_get_info_from_current_ip();
+        $lat = $user_location->location->latitude;
+        $lng = $user_location->location->longitude;
+        setcookie('user_location', $lat . ',' . $lng, time() + 3600);
+        return;
+    }
+
+    // Show notification to share location
+    echo '<p>Please share your location to access this website.</p>';
+    exit();
+}
+add_action('init', 'require_location');
 
 function has_shared_location() {
     // Check if user has shared location before
