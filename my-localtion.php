78ea69c85e85904d0d26dae0d095b1a6bfa50e55
hitcomvn <130174322+hitcomvn@users.noreply.github.com>
2023-04-09 15:16:51 +0700

    Update my-localtion.php

--- my-localtion.php
+++ my-localtion.php
@@ -74,5 +74,70 @@ function my_location_save_to_log( $location ) {
 
     file_put_contents( $log_path, $log_data, FILE_APPEND | LOCK_EX );
 }
+/**
+ * Save user location meta box data
+ *
+ * @param int $user_id The ID of the user being saved.
+ */
+function my_location_save_user_location_meta_box( $user_id ) {
+    // Make sure the current user can edit the user and the nonce is valid.
+    if ( ! current_user_can( 'edit_user', $user_id ) || ! isset( $_POST['my_location_nonce'] ) || ! wp_verify_nonce( $_POST['my_location_nonce'], 'my_location_save_user_location_meta_box' ) ) {
+        return;
+    }
+
+    // Get the user's latitude, longitude, address, and source from the form data.
+    $latitude  = isset( $_POST['my_location_latitude'] ) ? sanitize_text_field( $_POST['my_location_latitude'] ) : '';
+    $longitude = isset( $_POST['my_location_longitude'] ) ? sanitize_text_field( $_POST['my_location_longitude'] ) : '';
+    $address   = isset( $_POST['my_location_address'] ) ? sanitize_text_field( $_POST['my_location_address'] ) : '';
+    $source    = isset( $_POST['my_location_source'] ) ? sanitize_text_field( $_POST['my_location_source'] ) : '';
+
+    // Update the user's latitude, longitude, address, and source in the database.
+    update_user_meta( $user_id, 'my_location_latitude', $latitude );
+    update_user_meta( $user_id, 'my_location_longitude', $longitude );
+    update_user_meta( $user_id, 'my_location_address', $address );
+    update_user_meta( $user_id, 'my_location_source', $source );
+}
+
+/**
+ * Display user's location on the website
+ *
+ * @param boolean $use_geo_html_5 Whether or not to use geo HTML 5.
+ */
+function my_location_display_user_location( $use_geo_html_5 = true ) {
+    $latitude  = '';
+    $longitude = '';
+    $address   = '';
+    $source    = '';
+
+    if ( is_user_logged_in() ) {
+        $user_id   = get_current_user_id();
+        $latitude  = get_user_meta( $user_id, 'my_location_latitude', true );
+        $longitude = get_user_meta( $user_id, 'my_location_longitude', true );
+        $address   = get_user_meta( $user_id, 'my_location_address', true );
+        $source    = get_user_meta( $user_id, 'my_location_source', true );
+    }
+
+    if ( empty( $latitude ) && empty( $longitude ) && $use_geo_html_5 ) {
+        echo '<div id="my-location-message"></div>';
+    }
+
+    if ( ! empty( $latitude ) && ! empty( $longitude ) ) {
+        echo '<div id="my-location-map"></div>';
+        echo '<div id="my-location-address">' . $address . '</div>';
+        echo '<div id="my-location-source">' . $source . '</div>';
+    }
+}
+
+/**
+ * Update the user's location via AJAX
+ */
+function my_location_update_user_location() {
+    // Get the user's latitude and longitude from the AJAX request.
+    $latitude = isset( $_POST['latitude'] ) ? sanitize_text_field( $_POST['latitude'] ) : '';
+    $longitude = isset( $_POST['longitude'] ) ? sanitize_text_field( $_POST['longitude'] ) : '';
+
+    // Update the user's latitude and longitude in the database.
+    $user_id = get_current_user_id();
+    update_user_meta( $user_id, 'my_location_latitude', $latitude
 
 function my_location_init
